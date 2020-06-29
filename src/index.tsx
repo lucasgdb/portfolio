488376b5a41748db3bfc65168b2db3ee81e3a83f
Lucas Bittencourt <lucasgdbittencourt@gmail.com>
2020-06-28 23:21:50 -0300

    add: google analytics

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactGA from 'react-ga';
 import App from './App';
+
+ReactGA.initialize('UA-171095156-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(<App />, document.getElementById('root'));
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,20 @@
+import { createBrowserHistory } from 'history';
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import ReactGA from 'react-ga';
+import { Router } from 'react-router-dom';
 import AppRoutes from './app.route';
+
+const history = createBrowserHistory();
 
 export default function Routes() {
 	return (
-		<BrowserRouter>
+		<Router history={history}>
 			<AppRoutes />
-		</BrowserRouter>
+		</Router>
 	);
 }
+
+history.listen((location) => {
+	ReactGA.ga('set', 'page', location.pathname + location.search);
+	ReactGA.ga('send', 'pageview');
+});
