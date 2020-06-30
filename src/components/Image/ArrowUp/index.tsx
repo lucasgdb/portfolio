6b7a5eef48fd691307b05a5d3dbc34532fe7a24c
Lucas Bittencourt <lucasgdbittencourt@gmail.com>
2020-06-30 00:20:48 -0300

    update: arrow up color

--- src/components/Image/ArrowUp/index.tsx
+++ src/components/Image/ArrowUp/index.tsx
@@ -5,7 +5,7 @@ const ArrowUp = () => (
 		width="1em"
 		height="1em"
 		viewBox="0 0 16 16"
-		className="bi bi-arrow-up-circle-fill text-dark border-0"
+		className="bi bi-arrow-up-circle-fill text-danger border-0"
 		fill="currentColor"
 		xmlns="http://www.w3.org/2000/svg"
 	>
