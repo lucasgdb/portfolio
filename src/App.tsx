855155d209f13c404ae13544f206a5169d8cc1b1
Lucas Bittencourt <lucasgdbittencourt@gmail.com>
2020-07-01 14:10:46 -0300

    temporarily removed PWA

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,11 @@ import React from 'react';
 import { ReactGenieAnimations } from 'react-genie-styled-components';
 import NotificationProvider from '~/contexts/notification.context';
 import Routes from '~/routes';
-import ServiceWorkerWrapper from './components/ServiceWorkerWrapper';
 
 function App() {
 	return (
 		<NotificationProvider>
 			<ReactGenieAnimations />
-			<ServiceWorkerWrapper />
 
 			<Routes />
 		</NotificationProvider>
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ReactGA from 'react-ga';
+import * as serviceWorker from '~/serviceWorker';
 import App from './App';
 
 ReactGA.initialize('UA-171095156-1');
 ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+serviceWorker.unregister();
