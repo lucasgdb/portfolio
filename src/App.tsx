389c6fc5d0b80f40ce0c840df0e67a63ecda5c9b
Lucas Bittencourt <lucasgdbittencourt@gmail.com>
2020-07-01 18:30:32 -0300

    remove: react-toastify

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import { ReactGenieAnimations } from 'react-genie-styled-components';
-import NotificationProvider from '~/contexts/notification.context';
 import Routes from '~/routes';
 
 function App() {
 	return (
-		<NotificationProvider>
+		<>
 			<ReactGenieAnimations />
-
 			<Routes />
-		</NotificationProvider>
+		</>
 	);
 }
 
