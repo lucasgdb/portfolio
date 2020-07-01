5d7c17ed9fe9f7a7df6262cbc2394e62d066b427
Lucas Bittencourt <lucasgdbittencourt@gmail.com>
2020-07-01 01:38:19 -0300

    update: fixed footer & new contact

--- src/components/Image/index.tsx
+++ src/components/Image/index.tsx
@@ -1,7 +1,5 @@
 import ArrowDown from './ArrowDown';
 import ArrowUp from './ArrowUp';
-import GitHub from './GitHub';
 
 export { ArrowUp };
 export { ArrowDown };
-export { GitHub };
--- src/pages/Landing/Contact/index.tsx
+++ src/pages/Landing/Contact/index.tsx
@@ -1,6 +1,12 @@
 import React, { useRef } from 'react';
 import { Reveal } from 'react-genie';
 import { Animation } from 'react-genie-styled-components';
+import facebookIcon from '~/static/facebook.png';
+import githubIcon from '~/static/github.png';
+import linkedInIcon from '~/static/linkedin.png';
+import stackOverFlowIcon from '~/static/stackoverflow.png';
+import telegramIcon from '~/static/telegram.png';
+import whatsAppIcon from '~/static/whatsapp.png';
 
 const Contact = () => {
 	const form = useRef() as React.MutableRefObject<HTMLFormElement>;
@@ -37,57 +43,166 @@ const Contact = () => {
 				</p>
 			</Reveal>
 
-			<form ref={form} onSubmit={handleOnSubmit}>
-				<div className="container mt-5">
-					<Reveal animation={Animation.FadeInRight}>
-						<div className="mb-3">
-							<label htmlFor="name" className="form-label text-light">
-								Nome
-							</label>
+			<div className="container mb-4">
+				<div className="row">
+					<div className="col-12 col-md-6 mt-4 order-md-2">
+						<form ref={form} onSubmit={handleOnSubmit}>
+							<Reveal animation={Animation.FadeInRight}>
+								<div className="mb-3">
+									<label
+										htmlFor="name"
+										className="form-label text-light"
+									>
+										Nome
+									</label>
 
-							<input
-								type="text"
-								className="form-control"
-								id="name"
-								name="name"
-								placeholder="Ex: Lucas Bittencourt"
-								required
-							/>
+									<input
+										type="text"
+										className="form-control"
+										id="name"
+										name="name"
+										placeholder="Ex: Lucas Bittencourt"
+										required
+									/>
+								</div>
+
+								<div className="mb-3">
+									<label
+										htmlFor="email"
+										className="form-label text-light"
+									>
+										E-mail
+									</label>
+
+									<input
+										type="email"
+										className="form-control"
+										id="email"
+										name="email"
+										placeholder="nome@exemplo.com"
+										required
+									/>
+								</div>
+
+								<div className="mb-3">
+									<label
+										htmlFor="message"
+										className="form-label text-light"
+									>
+										Mensagem
+									</label>
+
+									<textarea
+										className="form-control"
+										id="message"
+										name="message"
+										rows={3}
+										required
+									></textarea>
+								</div>
+
+								<button className="btn btn-danger">Enviar</button>
+							</Reveal>
+						</form>
+					</div>
+
+					<div className="col-12 col-md-6 mt-4 order-md-1">
+						<div>
+							<div className="d-flex align-items-center">
+								<img src={linkedInIcon} alt="LinkedIn" width={30} />{' '}
+								<h4 className="text-light mb-0 ml-2">LinkedIn</h4>
+							</div>
+
+							<a
+								href="https://linkedin.com/in/lucas-bittencourt"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								/in/lucas-bittencourt
+							</a>
 						</div>
 
-						<div className="mb-3">
-							<label htmlFor="email" className="form-label text-light">
-								E-mail
-							</label>
+						<div className="mt-3">
+							<div className="d-flex align-items-center">
+								<img
+									src={stackOverFlowIcon}
+									alt="StackOverFlow"
+									width={30}
+								/>{' '}
+								<h4 className="text-light mb-0 ml-2">StackOverFlow</h4>
+							</div>
 
-							<input
-								type="email"
-								className="form-control"
-								id="email"
-								name="email"
-								placeholder="nome@exemplo.com"
-								required
-							/>
+							<a
+								href="https://pt.stackoverflow.com/users/93508/lucas-bittencourt"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								/lucas-bittencourt
+							</a>
 						</div>
 
-						<div className="mb-3">
-							<label htmlFor="message" className="form-label text-light">
-								Mensagem
-							</label>
+						<div className="mt-3">
+							<div className="d-flex align-items-center">
+								<img src={githubIcon} alt="GitHub" width={30} />{' '}
+								<h4 className="text-light mb-0 ml-2">GitHub</h4>
+							</div>
 
-							<textarea
-								className="form-control"
-								id="message"
-								name="message"
-								rows={3}
-								required
-							></textarea>
+							<a
+								href="https://github.com/lucasgdb"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								/lucasgdb
+							</a>
 						</div>
 
-						<button className="btn btn-danger">Enviar</button>
-					</Reveal>
+						<div className="mt-3">
+							<div className="d-flex align-items-center">
+								<img src={facebookIcon} alt="Facebook" width={30} />{' '}
+								<h4 className="text-light mb-0 ml-2">Facebook</h4>
+							</div>
+
+							<a
+								href="https://www.facebook.com/lucasgdbittencourt"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								/lucasgdbittencourt
+							</a>
+						</div>
+
+						<div className="mt-3">
+							<div className="d-flex align-items-center">
+								<img src={whatsAppIcon} alt="WhatsApp" width={30} />{' '}
+								<h4 className="text-light mb-0 ml-2">WhatsApp</h4>
+							</div>
+
+							<a
+								href="https://api.whatsapp.com/send?phone=5512988344336&text=Oi, Lucas!"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								+55 (12) 98834-4336
+							</a>
+						</div>
+
+						<div className="mt-3">
+							<div className="d-flex align-items-center">
+								<img src={telegramIcon} alt="Telegram" width={30} />{' '}
+								<h4 className="text-light mb-0 ml-2">Telegram</h4>
+							</div>
+
+							<a
+								href="tg://resolve?domain=lucasgdb"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								@lucasgdb
+							</a>
+						</div>
+					</div>
 				</div>
-			</form>
+			</div>
 		</div>
 	);
 };
