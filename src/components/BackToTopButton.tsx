d594ee9bf212aba961696aacc82c99f71c02e488
lucasgdb <lucasgdbittencourt@gmail.com>
2021-04-19 14:55:10 -0300

    perf(button): improving back to top button

---
+++ src/components/BackToTopButton.tsx
@@ -0,0 +1,60 @@
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
+
+const BackToTop = styled.div`
+  position: fixed;
+  width: 50px;
+  height: 50px;
+  right: 25px;
+  bottom: 50px;
+  cursor: pointer;
+`;
+
+const BackToTopButton: React.FC = () => {
+  const [show, setShow] = useState(false);
+  const [scrollTop, setScrollTop] = useState(
+    window.pageYOffset || document.documentElement.scrollTop
+  );
+
+  useEffect(() => {
+    setShow(scrollTop >= 30);
+  }, [scrollTop]);
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      const newScrollTop =
+        window.pageYOffset || document.documentElement.scrollTop;
+
+      setScrollTop(newScrollTop);
+    });
+  }, []);
+
+  const handleClick = () => {
+    const introductionElement: any = document.querySelector('#introduction');
+
+    window.scroll({
+      top: introductionElement?.offsetParent?.offsetTop,
+      behavior: 'smooth',
+    });
+  };
+
+  return show ? (
+    <BackToTop onClick={handleClick}>
+      <svg
+        width="100%"
+        height="100%"
+        viewBox="0 0 16 16"
+        className="bi bi-arrow-up-circle-fill text-danger border-0"
+        fill="currentColor"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          fillRule="evenodd"
+          d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-10.646.354a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 6.207V11a.5.5 0 0 1-1 0V6.207L5.354 8.354z"
+        />
+      </svg>
+    </BackToTop>
+  ) : null;
+};
+
+export default BackToTopButton;
--- src/pages/Landing/LandingPage.tsx
+++ src/pages/Landing/LandingPage.tsx
@@ -1,4 +1,4 @@
-import BackToTop from 'react-back-to-top-button';
+import BackToTopButton from '~/components/BackToTopButton';
 import Header from '~/components/Header';
 import Certificates from '~/components/Landing/Certificates/Certificates';
 import Contact from '~/components/Landing/Contact/Contact';
@@ -15,21 +15,7 @@ const LandingPage: React.FC = () => (
     <Contact />
     <Footer />
 
-    <BackToTop showAt={100} speed={1000} easing="easeInOutQuint">
-      <svg
-        width="1em"
-        height="1em"
-        viewBox="0 0 16 16"
-        className="bi bi-arrow-up-circle-fill text-danger border-0"
-        fill="currentColor"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          fillRule="evenodd"
-          d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-10.646.354a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 6.207V11a.5.5 0 0 1-1 0V6.207L5.354 8.354z"
-        />
-      </svg>
-    </BackToTop>
+    <BackToTopButton />
   </>
 );
 
