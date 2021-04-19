17940d322e4fec755264c6ff861ea8f8f8cb8e6e
lucasgdb <lucasgdbittencourt@gmail.com>
2021-04-19 15:25:12 -0300

    chore: add top button animation

--- src/components/BackToTopButton.tsx
+++ src/components/BackToTopButton.tsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from 'react';
+import { Reveal } from 'react-genie';
+import { Animation } from 'react-genie-styled-components';
 import styled from 'styled-components';
 
 const BackToTop = styled.div`
@@ -40,19 +42,21 @@ const BackToTopButton: React.FC = () => {
 
   return show ? (
     <BackToTop onClick={handleClick}>
-      <svg
-        width="100%"
-        height="100%"
-        viewBox="0 0 16 16"
-        className="bi bi-arrow-up-circle-fill text-danger border-0"
-        fill="currentColor"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          fillRule="evenodd"
-          d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-10.646.354a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 6.207V11a.5.5 0 0 1-1 0V6.207L5.354 8.354z"
-        />
-      </svg>
+      <Reveal animation={Animation.FadeInUp}>
+        <svg
+          width="100%"
+          height="100%"
+          viewBox="0 0 16 16"
+          className="bi bi-arrow-up-circle-fill text-danger border-0"
+          fill="currentColor"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fillRule="evenodd"
+            d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-10.646.354a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 6.207V11a.5.5 0 0 1-1 0V6.207L5.354 8.354z"
+          />
+        </svg>
+      </Reveal>
     </BackToTop>
   ) : null;
 };
