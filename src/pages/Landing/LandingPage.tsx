fe4af5736d4edd322d3bc815e45f0b380ea9710c
lucasgdb <lucasgdbittencourt@gmail.com>
2022-07-29 11:02:17 -0300

    fix: Header component location

--- src/pages/Landing/LandingPage.tsx
+++ src/pages/Landing/LandingPage.tsx
@@ -1,8 +1,8 @@
 import BackToTopButton from '~/components/BackToTopButton';
+import Header from '~/components/Header';
 import CertificateList from './components/Certificates/CertificateList';
 import Contact from './components/Contact/Contact';
 import Footer from './components/Footer/Footer';
-import Header from './components/Header';
 import Introduction from './components/Introduction';
 import MyTimeline from './components/MyTimeline/MyTimeline';
 
